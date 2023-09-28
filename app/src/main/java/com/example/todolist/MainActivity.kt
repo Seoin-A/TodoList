42495da16c7ebc0d81bf4b53897c004ed46fcbba
seoin2356 <seoin2389@icloud.com>
2023-09-28 22:07:46 +0900

    - view binding 추가 커밋

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -9,12 +9,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
-    private lateinit var btn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
     }
 }
