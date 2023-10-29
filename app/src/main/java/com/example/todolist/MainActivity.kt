ea92f50dd4609f1a25bed72e834b5ae487cd7b16
seoin <seoin2389@icloud.com>
2023-10-29 22:54:02 +0900

    - 삭제 기능 완성

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.todolist
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.databinding.ActivityMainBinding
@@ -46,7 +48,7 @@ class MainActivity : AppCompatActivity() , OnItemLongClickListener {
 
     private fun setRecyclerView(){
         runOnUiThread {
-            adapter = TodoRecyclerViewAdapter(todoList)
+            adapter = TodoRecyclerViewAdapter(todoList,this)
             binding.recyclerview.adapter = adapter
             binding.recyclerview.layoutManager = LinearLayoutManager(this)
         }
@@ -61,8 +63,25 @@ class MainActivity : AppCompatActivity() , OnItemLongClickListener {
         val builder : AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(getString(R.string.alert_title))
         builder.setMessage(getString(R.string.alert_message))
-        builder.setNegativeButton(getString(R.string.alert_no))
+        builder.setNegativeButton(getString(R.string.alert_no),null)
         builder.setPositiveButton(getString(R.string.alert_yes),
-            object)
+            object : DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    deleteTodo(position)
+                }
+            }
+        )
+        builder.show()
+    }
+
+    private fun deleteTodo(position: Int){
+        Thread{
+            todoDao.deleteTodo(todoList[position])
+            todoList.removeAt(position)
+            runOnUiThread{
+                adapter.notifyDataSetChanged()
+                Toast.makeText(this,"삭제되었습니다",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
     }
 }
